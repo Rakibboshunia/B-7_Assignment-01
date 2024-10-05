bd8f03a7aaa53aa618af6ac7dd24bcccb4f24265
MD. AL-RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2024-10-05 23:56:26 +0600

    index.js

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
 // 1. Function to calculate the difference between two numbers
-function calculateDifference(a, b) 
-{
+function calculateDifference(a, b){
     return a - b;
 }
 
 // 2. Function to check if a number is odd
-function isOdd(num) 
-{
+function isOdd(num){
     return num % 2 !== 0;
 }
 
 // 3. Function to find the minimum number in an array
-function findMin(arr)
-{
+function findMin(arr){
     return Math.min(...arr);
 }
 
 // 4. Function to filter even numbers from an array
-function filterEvenNumbers(arr) 
-{
+function filterEvenNumbers(arr){
     return arr.filter(num => num % 2 === 0);
 }
 
 // 5. Function to sort an array in descending order
-function sortArrayDescending(arr) 
-{
+function sortArrayDescending(arr){
     return arr.sort((a, b) => b - a);
 }
 
 // 6. Function to lowercase the first letter of a string
-function lowercaseFirstLetter(str) 
-{
+function lowercaseFirstLetter(str){
     return str.charAt(0).toLowerCase() + str.slice(1);
 }
 
 // 7. Function to count vowels in a string
-function countVowels(str) 
-{
+function countVowels(str){
     let vowels = 'aeiouAEIOU';
     return Array.from(str).filter(letter => vowels.includes(letter)).length;
 }
 
 // 8. Function to find the average of an array of numbers
-function findAverage(arr) 
-{
+function findAverage(arr){
     return arr.reduce((acc, num) => acc + num, 0) / arr.length;
 }
